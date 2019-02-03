51dabdb669da273b5cb02d9a6e531c9c7581d02a
Andrew's Rails Course <talle.andrew@gmail.com>
2019-02-02 22:01:19 -0500

    set up tools

--- fem/src/App.js
+++ fem/src/App.js
@@ -1,31 +1,41 @@
-const Pet = (props) => {
+
+
+const Pet = props => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
     React.createElement("h2", {}, props.animal),
     React.createElement("h2", {}, props.breed)
   ]);
+};
+
+class App extends React.Component {
+  handleTitleClick() {
+    alert("you clicked the title");
+  }
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement(
+        "h1",
+        { onClick: this.handleTitleClick },
+        "Adopt Me!"
+      ),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "dog",
+        breed: "Havanese"
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "bird",
+        breed: "Cockatiel"
+      }),
+      React.createElement(Pet, {
+        name: "Hope",
+        animal: "dog",
+        breed: "mini-doodle"
+      })
+    ]);
+  }
 }
 
-
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "dog",
-      breed: "Havanese"
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "bird",
-      breed: "Cockatiel"
-    }),
-    React.createElement(Pet, {
-      name: "Hope",
-      animal: "dog",
-      breed: "mini-doodle"
-    })
-  ])
-}
-
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
