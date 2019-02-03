614610bf32f430424a70356a7c5ca8b8c7cfdfe4
Andrew's Rails Course <talle.andrew@gmail.com>
2019-02-02 19:24:45 -0500

    first commit

---
+++ fem/src/App.js
@@ -0,0 +1,31 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+}
+
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement('h1', {}, 'Adopt Me!'),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "dog",
+      breed: "Havanese"
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "bird",
+      breed: "Cockatiel"
+    }),
+    React.createElement(Pet, {
+      name: "Hope",
+      animal: "dog",
+      breed: "mini-doodle"
+    })
+  ])
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'))
